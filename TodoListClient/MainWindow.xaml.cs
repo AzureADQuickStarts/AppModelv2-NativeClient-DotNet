f4d21f1893ab5c4865200c97bd5c7b9cb9090623
dstrockis <dastrock@microsoft.com>
2015-08-06 14:32:15 -0700

    Complete complete

---
+++ TodoListClient/MainWindow.xaml.cs
@@ -0,0 +1,283 @@
+﻿//----------------------------------------------------------------------------------------------
+//    Copyright 2014 Microsoft Corporation
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//----------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// The following using statements were added for this sample.
+using System.Globalization;
+using Microsoft.Experimental.IdentityModel.Clients.ActiveDirectory;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Script.Serialization;
+using System.Runtime.InteropServices;
+using System.Configuration;
+using Microsoft.Experimental.IdentityModel.Clients.ActiveDirectory;
+
+namespace TodoListClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //
+        // The Client ID is used by the application to uniquely identify itself to the v2.0 endpoint
+        // The AAD Instance is the instance of the v2.0 endpoint
+        // The Redirect URI is the URI where the v2.0 endpoint will return OAuth responses.
+        // The Authority is the sign-in URL.
+        //
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        Uri redirectUri = new Uri(ConfigurationManager.AppSettings["ida:RedirectUri"]);
+        private static string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, "common");
+        private static string todoListBaseAddress = ConfigurationManager.AppSettings["todo:TodoListBaseAddress"];
+
+        private HttpClient httpClient = new HttpClient();
+        private AuthenticationContext authContext = null;
+
+        protected override async void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            //
+            // As the application starts, try to get an access token without prompting the user.  If one exists, populate the To Do list.  If not, continue.
+            //
+            authContext = new AuthenticationContext(authority, new FileCache());
+            AuthenticationResult result = null;
+
+            try
+            {
+                result = await authContext.AcquireTokenAsync(new string[] { clientId }, null, clientId, redirectUri, new PlatformParameters(PromptBehavior.Never, null));
+
+                // A valid token is in the cache - get the To Do list.
+                SignInButton.Content = "Clear Cache";
+                GetTodoList();
+            }
+            catch (AdalException ex)
+            {
+                if (ex.ErrorCode == "user_interaction_required")
+                {
+                    // There are no tokens in the cache.  Proceed without calling the To Do list service.
+                }
+                else
+                {
+                    // An unexpected error occurred.
+                    string message = ex.Message;
+                    if (ex.InnerException != null)
+                    {
+                        message += "Inner Exception : " + ex.InnerException.Message;
+                    }
+                    MessageBox.Show(message);
+                }
+            }
+        }
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetTodoList()
+        {
+            //
+            // Get an access token to call the To Do service.
+            //
+            AuthenticationResult result = null;
+            try
+            {
+                result = await authContext.AcquireTokenAsync(new string[] { clientId }, null, clientId, redirectUri, new PlatformParameters(PromptBehavior.Never, null));
+            }
+            catch (AdalException ex)
+            {
+                // There is no access token in the cache, so prompt the user to sign-in.
+                if (ex.ErrorCode == "user_interaction_required")
+                {
+                    MessageBox.Show("Please sign in first");
+                    SignInButton.Content = "Sign In";
+                }
+                else
+                {
+                    // An unexpected error occurred.
+                    string message = ex.Message;
+                    if (ex.InnerException != null)
+                    {
+                        message += "Inner Exception : " + ex.InnerException.Message;
+                    }
+                    MessageBox.Show(message);
+                }
+
+                return;
+            }
+
+            // Once the token has been returned by ADAL, add it to the http authorization header, before making the call to access the To Do list service.
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.Token);
+
+            // Call the To Do list service.
+            HttpResponseMessage response = await httpClient.GetAsync(todoListBaseAddress + "/api/todolist");
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                // Read the response and databind to the GridView to display To Do items.
+                string s = await response.Content.ReadAsStringAsync();
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                List<TodoItem> toDoArray = serializer.Deserialize<List<TodoItem>>(s);
+
+                TodoList.ItemsSource = toDoArray.Select(t => new { t.Title });
+            }
+            else
+            {
+                MessageBox.Show("An error occurred : " + response.ReasonPhrase);
+            }
+
+            return;
+        }
+
+        private async void AddTodoItem(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TodoText.Text))
+            {
+                MessageBox.Show("Please enter a value for the To Do item name");
+                return;
+            }
+
+            //
+            // Get an access token to call the To Do service.
+            //
+            AuthenticationResult result = null;
+            try
+            {
+                result = await authContext.AcquireTokenAsync(new string[] { clientId }, null, clientId, redirectUri, new PlatformParameters(PromptBehavior.Never, null));
+            }
+            catch (AdalException ex)
+            {
+                // There is no access token in the cache, so prompt the user to sign-in.
+                if (ex.ErrorCode == "user_interaction_required")
+                {
+                    MessageBox.Show("Please sign in first");
+                    SignInButton.Content = "Sign In";
+                }
+                else
+                {
+                    // An unexpected error occurred.
+                    string message = ex.Message;
+                    if (ex.InnerException != null)
+                    {
+                        message += "Inner Exception : " + ex.InnerException.Message;
+                    }
+
+                    MessageBox.Show(message);
+                }
+
+                return;
+            }
+
+            //
+            // Call the To Do service.
+            //
+
+            // Once the token has been returned by ADAL, add it to the http authorization header, before making the call to access the To Do service.
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.Token);
+
+            // Forms encode Todo item, to POST to the todo list web api.
+            HttpContent content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("Title", TodoText.Text) });
+
+            // Call the To Do list service.
+            HttpResponseMessage response = await httpClient.PostAsync(todoListBaseAddress + "/api/todolist", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                TodoText.Text = "";
+                GetTodoList();
+            }
+            else
+            {
+                MessageBox.Show("An error occurred : " + response.ReasonPhrase);
+            }
+        }
+
+        private async void SignIn(object sender = null, RoutedEventArgs args = null)
+        {
+            // If there is already a token in the cache, clear the cache and update the label on the button.
+            if (SignInButton.Content.ToString() == "Clear Cache")
+            {
+                TodoList.ItemsSource = string.Empty;
+                authContext.TokenCache.Clear();
+                // Also clear cookies from the browser control.
+                ClearCookies();
+                SignInButton.Content = "Sign In";
+                return;
+            }
+
+            //
+            // Get an access token to call the To Do list service.
+            //
+            AuthenticationResult result = null;
+            try
+            {
+                result = await authContext.AcquireTokenAsync(new string[] { clientId }, null, clientId, redirectUri, new PlatformParameters(PromptBehavior.Always, null));
+                SignInButton.Content = "Clear Cache";
+                GetTodoList();
+            }
+            catch (AdalException ex)
+            {
+                if (ex.ErrorCode == "authentication_canceled")
+                {
+                    MessageBox.Show("Sign in was canceled by the user");
+                }
+                else
+                {
+                    // An unexpected error occurred.
+                    string message = ex.Message;
+                    if (ex.InnerException != null)
+                    {
+                        message += "Inner Exception : " + ex.InnerException.Message;
+                    }
+
+                    MessageBox.Show(message);
+                }
+
+                return;
+            }
+
+        }
+
+        // This function clears cookies from the browser control used by ADAL.
+        private void ClearCookies()
+        {
+            const int INTERNET_OPTION_END_BROWSER_SESSION = 42;
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_END_BROWSER_SESSION, IntPtr.Zero, 0);
+        }
+
+        [DllImport("wininet.dll", SetLastError = true)]
+        private static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int lpdwBufferLength);
+
+    }
+}
---
+++ TodoListService/App_Start/Startup.Auth.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Microsoft.Owin.Security;
+using Owin;
+using System.IdentityModel.Tokens;
+using TodoListService.App_Start;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security.OAuth;
+
+namespace TodoListService
+{
+    public partial class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:Audience"];
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            var tvps = new TokenValidationParameters
+            {
+                // The web app and the service are sharing the same clientId
+                ValidAudience = clientId,
+                ValidateIssuer = false,
+            };
+
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions
+            {
+                AccessTokenFormat = new Microsoft.Owin.Security.Jwt.JwtFormat(tvps, new OpenIdConnectCachingSecurityTokenProvider("https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration")),
+            });
+        }
+    }
+}
